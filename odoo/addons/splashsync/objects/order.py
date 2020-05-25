74a2985d828f31334f7d212e8de26494751ec16a
olivier bouzon <oliv@hotmail.fr>
2020-05-25 16:44:32 +0200

    Order & Invoice Objects

--- odoo/addons/splashsync/objects/order.py
+++ odoo/addons/splashsync/objects/order.py
@@ -30,7 +30,7 @@ class Order(OdooObject, Orderlines):
     @staticmethod
     def get_listed_fields():
         """Get List of Object Fields to Include in Lists"""
-        return ['client_order_ref', 'name', 'date_order', 'type_name']
+        return ['name', 'display_name', 'display_name', 'client_order_ref', 'id']
 
     @staticmethod
     def get_required_fields():
@@ -39,17 +39,29 @@ class Order(OdooObject, Orderlines):
             # 'name', 'date_order', 'currency_id',
             # 'partner_id', 'partner_invoice_id', 'partner_shipping_id',
             # 'pricelist_id', 'warehouse_id', 'picking_policy'
-        ]
+            'company_id', 'currency_id', 'journal_id'
+                ]
 
     @staticmethod
     def get_configuration():
         """Get Hash of Fields Overrides"""
         return {
+                "name": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "name"},
+                "state": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "paymentStatus"},
 
-            "activity_summary": {"write": False},
+                "description": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "description"},
+                "date_due": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "paymentDueDate"},
+                "date_invoice": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "dateCreated"},
+                "reference": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "confirmationNumber"},
 
-            "create_date": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateCreated"},
-            "write_date": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateModified"},
+                "create_date": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateCreated"},
+                "__last_update": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateModified"},
+
+                # "account.invoice.line[invoice_id]": {"group": "General", "itemtype": "http://schema.org/Invoice", "itemprop": "name"},
+
+                "activity_summary": {"write": False},
+
+                "write_date": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateModified"},
 
          }
 
