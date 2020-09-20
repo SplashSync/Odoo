df0c3f3e82679f02342c1d16d1c402d7b8d9e2a2
olivier bouzon <oliv@hotmail.fr>
2020-09-20 20:58:39 +0200

    isaddress & is

--- odoo/addons/splashsync/objects/addresses/addrname.py
+++ odoo/addons/splashsync/objects/addresses/addrname.py
@@ -21,65 +21,84 @@ class AddrName:
     Manage Encode/Decode Concatenated AddrName
     """
 
-    varfullname = {"first": "", "last": ""}
+    addr_fullname_buffer = {}
 
     def buildAddrNameFields(self):
         FieldFactory.create(const.__SPL_T_VARCHAR__, "last", "Last Name")
         FieldFactory.microData("http://schema.org/Person", "givenName")
         FieldFactory.association('first')
-        # FieldFactory.isNotTested()
         # ==================================================================== #
         FieldFactory.create(const.__SPL_T_VARCHAR__, "first", "First Name")
         FieldFactory.microData("http://schema.org/Person", "familyName")
         FieldFactory.isRequired()
-        # FieldFactory.isNotTested()
 
     def getAddrNameFields(self, index, field_id):
-        if field_id == "first":
-            self._out[field_id] = AddrName.decodefullname(self)[field_id]
-            self._in.__delitem__(index)
-        if field_id == "last":
-            self._out[field_id] = AddrName.decodefullname(self)[field_id]
-            self._in.__delitem__(index)
+        # ==================================================================== #
+        # Filter on Field Id
+        if field_id not in ["first", "last"]:
+            return
+        # ==================================================================== #
+        # Read & Decode Field Data
+        self._out[field_id] = self.decodefullname()[field_id]
+        self._in.__delitem__(index)
 
     def setAddrNameFields(self, field_id, field_data):
+        # ==================================================================== #
+        # Filter on Field Id
+        if field_id not in ["first", "last"]:
+            return
+        # ==================================================================== #
+        # Safety Check
         if field_data is None:
             field_data = ""
-        if field_id == "first":
-            AddrName.varfullname[field_id] = AddrName.decodefullname(self)[field_id]
-            if AddrName.isactualdatadifferent(self, field_id, field_data):
-                AddrName.varfullname[field_id] = str(field_data).strip()
-            self._in.__delitem__(field_id)
-        if field_id == "last":
-            AddrName.varfullname[field_id] = AddrName.decodefullname(self)[field_id]
-            if AddrName.isactualdatadifferent(self, field_id, field_data):
-                AddrName.varfullname[field_id] = str(field_data).strip()
-            self._in.__delitem__(field_id)
+        # ==================================================================== #
+        # WRITE Field Data in Buffer
+        self.addr_fullname_buffer[field_id] = str(field_data).strip()
+        self._in.__delitem__(field_id)
+        # ==================================================================== #
+        # Once all Fullname's Fields are Parsed, then Encode & Set Fullname
         if all(x not in self._in for x in ["first", "last"]):
-            setattr(self.object, "name", AddrName.encodefullname(AddrName.varfullname["first"], AddrName.varfullname["last"]))
+            setattr(self.object, "name", self.encodefullname())
 
     def decodefullname(self):
+        """
+        Decode Last Name & First Name from Contact Name
+        :return: dict
+        """
+        # ==================================================================== #
+        # Init dict
         elements = {"first": "", "last": ""}
+        # ==================================================================== #
+        # Get Fullname to decode
         fullname = str(getattr(self.object, "name")).strip()
-        if fullname is None:
-            return
+        # ==================================================================== #
+        # Decode
         if ", " not in fullname:
             elements["first"] = fullname.strip()
         else:
             elements["first"] = fullname.split(", ")[0].strip()
             elements["last"] = fullname.split(", ")[-1].strip()
+
         return elements
 
-    @staticmethod
-    def encodefullname(first, last):
+    def encodefullname(self):
+        """
+        Encode Last Name & First Name from Buffer
+        :return: str
+        """
+        # ==================================================================== #
+        # Init return as empty str
         result = ""
+        # ==================================================================== #
+        # Get Data To Encode
+        first = self.addr_fullname_buffer["first"]
+        last = self.addr_fullname_buffer["last"]
+        # ==================================================================== #
+        # Encode
         if (isinstance(first, str)) and (len(first) > 0):
             if (isinstance(last, str)) and (len(last) > 0):
-                result = first + ", " + last   # first, last
+                result = first + ", " + last                    # first, last
             else:
-                result = first                 # first
-        return result
+                result = first                                  # first
 
-    def isactualdatadifferent(self, field_id, field_data):
-        actualfield = AddrName.decodefullname(self)[field_id]
-        return field_data != actualfield
+        return result                                           # ""
