2b1275686237df39fd674f2ab051be57987a5fee
BadPixxel <eshop.bpaquier@gmail.com>
2022-03-14 13:35:16 +0100

    WIP: Multi-versions Module

--- ci/start_odoo.sh
+++ ci/start_odoo.sh
@@ -25,5 +25,17 @@ set -e
 title "TEST --> Start Toolkit"
 docker-compose exec -T toolkit php bin/console
 title "TEST --> Start Odoo"
-docker-compose exec -T toolkit bash -c 'cpt=0; while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' odoo:8069)" != "200" ]]; do ((cpt+=1)); echo "$cpt : Wait for Odoo..."; sleep 10; if [$cpt -eq 6] then exit 1; fi done'
+cpt=0;
+while [[ "$(docker-compose exec -T toolkit bash -c 'curl -s -o /dev/null -w ''%{http_code}'' odoo:80')" != "200" ]];
+do
+  ((cpt+=1));
+  echo "$cpt : Wait for Odoo...";
+  docker-compose logs --tail="100" odoo
+  sleep 10;
+  if [$cpt -eq 6]
+  then
+      echo "Odoo takes too long to start...";
+      exit 1;
+  fi
+done
 docker-compose logs --tail="2000" odoo >> logs/odoo.init.txt
