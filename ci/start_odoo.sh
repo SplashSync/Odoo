2c5ebc58c592e4dc3d5cbda26b219fa7e2ca7f8b
BadPixxel <eshop.bpaquier@gmail.com>
2022-03-14 14:29:58 +0100

    WIP: Multi-versions Module

--- ci/start_odoo.sh
+++ ci/start_odoo.sh
@@ -30,7 +30,7 @@ title "TEST --> Start Odoo"
 cpt=0;
 http_code="000";
 subtitle "$cpt => $http_code: Wait for Odoo...";
-while [[ $http_code != "200" ]]
+while [ "$http_code" != "200" ]
 do
   http_code=$(docker-compose exec -T toolkit bash -c 'curl -s -o /dev/null -w ''%{http_code}'' odoo:80');
   cpt=$(( cpt+1 ));
