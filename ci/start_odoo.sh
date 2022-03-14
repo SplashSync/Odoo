bbfe3339a273fac9aea937b645c159302e286809
BadPixxel <eshop.bpaquier@gmail.com>
2022-03-14 14:01:56 +0100

    WIP: Multi-versions Module

--- ci/start_odoo.sh
+++ ci/start_odoo.sh
@@ -21,23 +21,26 @@ set -e
 # import Layout Functions
 . scripts/functions.sh
 ################################################################
-# Wait Until Odoo Container Started
+# Ensure Toolkit Started
 title "TEST --> Start Toolkit"
 docker-compose exec -T toolkit php bin/console
+################################################################
+# Wait Until Odoo Container Started
 title "TEST --> Start Odoo"
 cpt=0;
-docker-compose exec -T toolkit bash -c 'curl -s -o /dev/null -w ''%{http_code}'' odoo:80'
-while [[ "$(docker-compose exec -T toolkit bash -c 'curl -s -o /dev/null -w ''%{http_code}'' odoo:80')" != "200" ]];
+http_code="000";
+while [ $http_code != "200" ];
 do
-  ((cpt+=1));
-  echo "$cpt : Wait for Odoo...";
+  http_code=$(docker-compose exec -T toolkit bash -c 'curl -s -o /dev/null -w ''%{http_code}'' odoo:80');
+  cpt=$(( cpt+1 ));
+  subtitle "$cpt => $http_code: Wait for Odoo...";
   docker-compose logs --tail="100" odoo
-  sleep 10;
-  if [$cpt -eq 6]
+  sleep 1;
+  if [ $cpt == '6' ];
   then
-      echo "Odoo takes too long to start...";
+      subtitle "Odoo takes too long to start...";
       exit 1;
   fi
 done
-title "TEST --> Odoo Started !"
+subtitle "TEST --> Odoo Started !"
 docker-compose logs --tail="2000" odoo >> logs/odoo.init.txt
