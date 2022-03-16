3b30f0ec0843cd23bf8a7d7f98ec27c6f550d289
BadPixxel <eshop.bpaquier@gmail.com>
2022-03-16 07:03:59 +0100

    WIP: Multi-versions Module

--- ci/update_images.sh
+++ ci/update_images.sh
@@ -21,20 +21,53 @@ set -e
 # import Layout Functions
 . scripts/functions.sh
 
-title "[$CI_REGISTRY_IMAGE:$ODOO_VERSION] Build & Upload Docker Image"
+################################################################################
+# Load Docker Images from Cache or Registry
+################################################################################
+#DB_ARGS=()
+#function check_db_config() {
+#    param="$1"
+#    value="$2"
+#    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
+#        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+#    fi;
+#    DB_ARGS+=("--${param}")
+#    DB_ARGS+=("${value}")
+#}
+
+title "[$CI_REGISTRY_IMAGE:$ODOO_VERSION] Build & Upload Docker Images"
 ################################################################
 # Connect Docker to GitLab
 subtitle "Connect Docker to GitLab"
 docker login -u gitlab-ci-token -p $CI_BUILD_TOKEN registry.gitlab.com
-################################################################
-# Build & Upload Odoo Docker Image
-subtitle "Build & Upload Odoo Docker Image"
-sed -i "s|_ODOO_VERSION_|${ODOO_VERSION}|g" ci/Dockerfile
-docker build -t $CI_REGISTRY_IMAGE:$ODOO_VERSION ci
-docker push $CI_REGISTRY_IMAGE:$ODOO_VERSION
-################################################################
-# Build & Upload Postgres Docker Image
-subtitle "Build & Upload Postgres Docker Image"
+
+#if [ -f "$FAST_BOOT" ]; then
+#  echo "[ODOO BOOT] Normal Mode"
+#  check_odoo_config "init" "$ODOO_MODULES"
+#else
+#  echo "[ODOO BOOT] FAST Mode"
+#fi
+mkdir -p "images"
+subtitle "Before Push"
+ls -l ./images
+docker image ls
+
 docker pull postgres:10
-docker image tag postgres:10 $CI_REGISTRY_IMAGE:db
-docker push $CI_REGISTRY_IMAGE:db
+docker save -o images/postgres.10.tar
+
+subtitle "After Push"
+ls -l ./images
+docker image ls
+
+#################################################################
+## Build & Upload Odoo Docker Image
+#subtitle "Build & Upload Odoo Docker Image"
+#sed -i "s|_ODOO_VERSION_|${ODOO_VERSION}|g" ci/Dockerfile
+#docker build -t $CI_REGISTRY_IMAGE:$ODOO_VERSION ci
+#docker push $CI_REGISTRY_IMAGE:$ODOO_VERSION
+#################################################################
+## Build & Upload Postgres Docker Image
+#subtitle "Build & Upload Postgres Docker Image"
+#docker pull postgres:10
+#docker image tag postgres:10 $CI_REGISTRY_IMAGE:db
+#docker push $CI_REGISTRY_IMAGE:db
