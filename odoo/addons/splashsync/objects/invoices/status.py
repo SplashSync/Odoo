faaaa5bd1c5618a0ffc2387335327dbf70df0b94
BadPixxel <eshop.bpaquier@gmail.com>
2022-07-06 14:31:51 +0200

    WIP: Invoices

--- odoo/addons/splashsync/objects/invoices/status.py
+++ odoo/addons/splashsync/objects/invoices/status.py
@@ -21,22 +21,6 @@ class InvoiceStatus:
 
     __new_state = None
 
-    __known_state = {
-        'draft': 'Draft',
-        'open': 'Open',
-        'in_payment': 'In Payment',
-        'paid': 'Paid',
-        'cancel': 'Cancelled',
-    }
-
-    __known_state_trans = {
-        'draft': 'PaymentDraft',
-        'open': 'PaymentDue',
-        'in_payment': 'PaymentDue',
-        'paid': 'PaymentComplete',
-        'cancel': 'PaymentCanceled',
-    }
-
     def buildStatusFields(self):
         """
         Build Invoices Status Fields
@@ -44,10 +28,10 @@ class InvoiceStatus:
         :return: void
         """
         # ====================================================================#
-        # Order Global State
+        # Invoice Global State
         FieldFactory.create(const.__SPL_T_VARCHAR__, "state", "Invoice Status")
         FieldFactory.microData("http://schema.org/Invoice", "paymentStatus")
-        FieldFactory.addChoices(InvoiceStatus.__get_status_choices())
+        FieldFactory.addChoices(InvoiceStatus.get_helper().get_status_choices())
         FieldFactory.association("name@lines", "quantity@lines", "price_unit@lines")
         FieldFactory.group("General")
         # ====================================================================#
@@ -86,23 +70,23 @@ class InvoiceStatus:
         # ====================================================================#
         # Invoice Global State
         if field_id == "state":
-            self._out[field_id] = self.__spl_state(self.object.state)
+            self._out[field_id] = self.get_helper().to_splash(self.object)
             self._in.__delitem__(index)
         # Invoice is Draft
         if field_id == "isDraft":
-            self._out[field_id] = (self.object.state in ["draft"])
+            self._out[field_id] = self.get_helper().is_draft(self.object)
             self._in.__delitem__(index)
         # Invoice is Validated
         if field_id == "isValidated":
-            self._out[field_id] = (self.object.state in ["in_payment", "paid"])
+            self._out[field_id] = self.get_helper().is_validated(self.object)
             self._in.__delitem__(index)
         # Invoice is Closed
         if field_id == "isPaid":
-            self._out[field_id] = (self.object.state in ["paid"])
+            self._out[field_id] = self.get_helper().is_paid(self.object)
             self._in.__delitem__(index)
         # Invoice is Canceled
         if field_id == "isCanceled":
-            self._out[field_id] = (self.object.state in ["cancel"])
+            self._out[field_id] = self.get_helper().is_canceled(self.object)
             self._in.__delitem__(index)
 
     def setStatusFields(self, field_id, field_data):
@@ -117,8 +101,24 @@ class InvoiceStatus:
         # New Invoice State
         if field_id == "state":
             #  Translate State
-            self.__new_state = self.__odoo_state(field_data)
+            self.__new_state = self.get_helper().to_odoo(field_data)
             self._in.__delitem__(field_id)
+
+    @staticmethod
+    def get_helper():
+        """
+        Get Adapted Invoices Status Helper
+
+        :rtype: Odoo12StatusHelper|Odoo13StatusHelper
+        """
+
+        from odoo.addons.splashsync.helpers import SystemManager
+        if SystemManager.compare_version(12) == 0:
+            from odoo.addons.splashsync.helpers.objects.invoices.statusV12 import Odoo12StatusHelper
+            return Odoo12StatusHelper
+        elif SystemManager.compare_version(13) >= 0:
+            from odoo.addons.splashsync.helpers.objects.invoices.statusV13 import Odoo13StatusHelper
+            return Odoo13StatusHelper
 
     def post_set_status(self):
         """
@@ -141,16 +141,17 @@ class InvoiceStatus:
         if not InvoiceStatus.__is_allowed_status_change(self.object.state, new_state):
             Framework.log().warn('Invoice State Skipped')
             return True
+        StatusHelper = InvoiceStatus.get_helper()
         # ====================================================================#
         # Update State
         try:
-            self._set_status(new_state)
+            StatusHelper.set_status(self.object, new_state)
         except UserError as exception:
             return Framework.log().fromException(exception, False)
         # ====================================================================#
         # Verify Splash States
-        new_state = InvoiceStatus.__spl_state(new_state)
-        inv_state = InvoiceStatus.__spl_state(self.object.state)
+        new_state = StatusHelper.to_splash(new_state)
+        inv_state = StatusHelper.to_splash(self.object.state)
         if new_state == inv_state:
             return True
         # ====================================================================#
@@ -168,116 +169,23 @@ class InvoiceStatus:
         """
         # ====================================================================#
         # Check if Invoice is Draft
-        if self.object.state not in ['draft']:
+        if not InvoiceStatus.get_helper().is_draft(self.object):
             return True
         # ====================================================================#
         # Check if New State received but NOT parsed
         if "state" in self._in and isinstance(self._in['state'], str):
-            self.__new_state = self.__odoo_state(self._in['state'])
+            self.__new_state = InvoiceStatus.get_helper().to_odoo(self._in['state'])
         # ====================================================================#
         # Check New State is Valid
         if not isinstance(self.__new_state, str):
             return False
-        if self.__new_state not in ['open', 'in_payment', 'paid']:
+        if self.__new_state not in ['open', 'posted', 'in_payment', 'paid']:
             return False
         # ====================================================================#
         # Update State DRAFT => OPEN
-        self._set_status('open')
+        InvoiceStatus.get_helper().set_validated(self.object)
 
         return True
-
-    def _is_editable(self, state=None):
-        """
-        Check if Order Status is Editable
-
-        :rtype: bool
-        """
-        if state is None:
-            return self.object.state == "draft"
-        return state == "draft"
-
-    def _set_status(self, new_state):
-        """
-        Really set Invoice Status
-        :rtype: bool
-        """
-        # ====================================================================#
-        # IS Cancel
-        if new_state == 'cancel' and self.object.state in ['draft', 'open', 'in_payment', 'paid']:
-            if Framework.isDebugMode():
-                self.object.journal_id.update_posted = True
-            self.object.action_invoice_cancel()
-            self.object.refresh()
-        # ====================================================================#
-        # NOT Cancel
-        if self.object.state == 'cancel' and new_state in ['draft', 'open', 'in_payment', 'paid']:
-            self.object.action_invoice_draft()
-            self.object.refresh()
-        # ====================================================================#
-        # IS Draft
-        if new_state == 'draft':
-            # ====================================================================#
-            # TRY Validated => Cancel (Require Journal Update)
-            if self.object.state in ['open', 'in_payment', 'paid']:
-                if Framework.isDebugMode():
-                    self.object.journal_id.update_posted = True
-                self.object.action_invoice_cancel()
-                self.object.refresh()
-            # ====================================================================#
-            # Cancel => Draft
-            if self.object.state in ['cancel']:
-                self.object.action_invoice_draft()
-                self.object.refresh()
-        # ====================================================================#
-        # Draft => Open
-        if self.object.state == 'draft' and new_state in ['open', 'in_payment', 'paid']:
-            self.object.action_invoice_open()
-            self.object.refresh()
-        # ====================================================================#
-        # Open => Paid
-        if self.object.state == 'open' and new_state in ['in_payment', 'paid']:
-            self.object.action_invoice_paid()
-            self.object.refresh()
-
-    @staticmethod
-    def __spl_state(state):
-        """
-        Get Translated Order Status
-
-        :param state: str
-        :rtype: str
-        """
-        if state in InvoiceStatus.__known_state_trans.keys():
-            return InvoiceStatus.__known_state_trans[state]
-        return ""
-
-    @staticmethod
-    def __odoo_state(state):
-        """
-        Get Odoo Order Status
-
-        :param state: str
-        :rtype: str|None
-        """
-        for odoo_state, splash_state in InvoiceStatus.__known_state_trans.items():
-            if state == splash_state:
-                return odoo_state
-        return None
-
-    @staticmethod
-    def __get_status_choices():
-        """
-        Get List Of Possible Order Status Choices
-
-        :rtype: dict
-        """
-        response = []
-        for status, name in InvoiceStatus.__known_state.items():
-            if Framework.isDebugMode() and status in ['in_payment', 'paid']:
-                continue
-            response.append((InvoiceStatus.__known_state_trans[status], name))
-
-        return response
 
     @staticmethod
     def __is_allowed_status_change(old_status, new_status):
@@ -297,10 +205,10 @@ class InvoiceStatus:
             return True
         # ====================================================================#
         # Check if Old/Current Status is Validated Status
-        if old_status in ['open', 'in_payment', 'paid']:
+        if old_status in ['open', 'posted', 'in_payment', 'paid']:
             return True
         # ====================================================================#
         # Check if New Status is Validated Status
-        if new_status in ['open', 'in_payment', 'paid']:
+        if new_status in ['open', 'posted', 'in_payment', 'paid']:
             return False
         return True
