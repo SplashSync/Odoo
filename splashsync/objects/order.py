2d7357795c8c1dd147297f61589e063ee894d974
BadPixxel <eshop.bpaquier@gmail.com>
2024-12-02 13:24:38 +0100

    V16 - WIP CI/CD

--- splashsync/objects/order.py
+++ splashsync/objects/order.py
@@ -66,8 +66,10 @@ class Order(OdooObject, SafeName, OrderCore, OrderAddress, OrderCarrier, OrderDe
             "create_date": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateCreated"},
             "write_date": {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateModified"},
 
-            "activity_summary": {"write": False},
-            "picking_policy": {"required": False},
+            "activity_summary":     {"write": False},
+            "show_json_popover":    {"write": False},
+            "show_update_fpos":     {"write": False},
+            "picking_policy":       {"required": False},
         }
 
     # ====================================================================#
