0bbd4552c9a53740181754643c9db8aa6ad0a3b9
BadPixxel <eshop.bpaquier@gmail.com>
2025-10-08 19:28:31 +0200

    V18 - Refactor InvoiceStatusHelper to modernize methods and remove legacy version checks

--- splashsync/helpers/objects/invoices/status.py
+++ splashsync/helpers/objects/invoices/status.py
@@ -20,6 +20,20 @@ class InvoiceStatusHelper:
     Odoo Invoice Status Helper
     """
 
+    __known_state = {
+        'cancel': 'Cancelled',
+        'draft': 'Draft',
+        'posted': 'Posted',
+        'paid': 'Paid',
+    }
+
+    __known_state_trans = {
+        'cancel': 'PaymentCanceled',
+        'draft': 'PaymentDraft',
+        'posted': 'PaymentDue',
+        'paid': 'PaymentComplete',
+    }
+
     # ====================================================================#
     # Invoice State Getters
     # ====================================================================#
@@ -53,7 +67,7 @@ class InvoiceStatusHelper:
         :param invoice:
         :return: bool
         """
-        return InvoiceStatusHelper.__get_helper().is_validated(invoice)
+        return invoice.state in ["posted"]
 
     @staticmethod
     def is_paid(invoice):
@@ -63,7 +77,12 @@ class InvoiceStatusHelper:
         :param invoice:
         :return: bool
         """
-        return InvoiceStatusHelper.__get_helper().is_paid(invoice)
+        if not InvoiceStatusHelper.is_validated(invoice):
+            return False
+        try:
+            return invoice.payment_state in ["paid"]
+        except Exception:
+            return invoice.invoice_payment_state in ["paid"]
 
     @staticmethod
     def is_editable(invoice):
@@ -86,7 +105,7 @@ class InvoiceStatusHelper:
         :param invoice: account.invoice
         :rtype: None
         """
-        InvoiceStatusHelper.__get_helper().set_validated(invoice)
+        InvoiceStatusHelper.set_status(invoice, 'posted')
 
     @staticmethod
     def set_status(invoice, new_state):
@@ -97,7 +116,30 @@ class InvoiceStatusHelper:
 
         :rtype: bool
         """
-        return InvoiceStatusHelper.__get_helper().set_status(invoice, new_state)
+        # ====================================================================#
+        # IS Cancel
+        if new_state == 'cancel' and invoice.state in ['draft', 'posted']:
+            invoice.button_cancel()
+        # ====================================================================#
+        # NOT Cancel
+        if invoice.state == 'cancel' and new_state in ['draft', 'posted']:
+            invoice.button_draft()
+        # ====================================================================#
+        # IS Draft
+        if new_state == 'draft':
+            # ====================================================================#
+            # TRY Validated => Cancel (Require Journal Update)
+            if invoice.state in ['posted']:
+                invoice.button_cancel()
+            # ====================================================================#
+            # Cancel => Draft
+            if invoice.state in ['cancel']:
+                invoice.button_draft()
+        # ====================================================================#
+        # Draft => Posted
+        if invoice.state == 'draft' and new_state in ['posted', 'paid']:
+            invoice.action_post()
+            invoice._compute_amount()
 
     # ====================================================================#
     # Splash Methods
@@ -111,7 +153,16 @@ class InvoiceStatusHelper:
         :param invoice_or_state: account.invoice|str
         :rtype: str
         """
-        return InvoiceStatusHelper.__get_helper().to_splash(invoice_or_state)
+        if isinstance(invoice_or_state, str):
+            state = invoice_or_state
+        else:
+            if InvoiceStatusHelper.is_paid(invoice_or_state):
+                return "PaymentComplete"
+            state = invoice_or_state.state
+
+        if state in InvoiceStatusHelper.__known_state_trans.keys():
+            return InvoiceStatusHelper.__known_state_trans[state]
+        return ""
 
     @staticmethod
     def to_odoo(state):
@@ -121,7 +172,10 @@ class InvoiceStatusHelper:
         :param state: str
         :rtype: str|None
         """
-        return InvoiceStatusHelper.__get_helper().to_odoo(state)
+        for odoo_state, splash_state in InvoiceStatusHelper.__known_state_trans.items():
+            if state == splash_state:
+                return odoo_state
+        return None
 
     @staticmethod
     def get_status_choices():
@@ -130,23 +184,11 @@ class InvoiceStatusHelper:
 
         :rtype: dict
         """
-        return InvoiceStatusHelper.__get_helper().get_status_choices()
+        response = []
+        for status, name in InvoiceStatusHelper.__known_state.items():
+            response.append((
+                InvoiceStatusHelper.__known_state_trans[status],
+                '['+InvoiceStatusHelper.__known_state_trans[status]+'] '+name
+            ))
 
-    # ====================================================================#
-    # Private Methods
-    # ====================================================================#
-
-    @staticmethod
-    def __get_helper():
-        """
-        Get Adapted Invoices Status Helper
-
-        :rtype: OdooV12StatusHelper|OdooV13StatusHelper
-        """
-        from odoo.addons.splashsync.helpers import SystemManager
-        if SystemManager.compare_version(13) >= 0:
-            from odoo.addons.splashsync.helpers.objects.invoices.V13 import OdooV13StatusHelper
-            return OdooV13StatusHelper
-        elif SystemManager.compare_version(12) >= 0:
-            from odoo.addons.splashsync.helpers.objects.invoices.V12 import OdooV12StatusHelper
-            return OdooV12StatusHelper
+        return response
