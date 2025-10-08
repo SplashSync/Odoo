cf7de1812bd60a8088dba948aba4cedf1d005a91
BadPixxel <eshop.bpaquier@gmail.com>
2025-10-08 19:30:23 +0200

    Add URL prefix option for website field configuration

--- splashsync/objects/address.py
+++ splashsync/objects/address.py
@@ -89,7 +89,14 @@ class Address(OdooObject, PartnersCountry, AddresseName, PartnersParent, Address
             "create_date":                  {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateCreated"},
             "write_date":                   {"group": "Meta", "itemtype": "http://schema.org/DataFeedItem", "itemprop": "dateModified"},
 
-            "website":                      {"group": "", "type": const.__SPL_T_URL__, "itemtype": "http://schema.org/Organization", "itemprop": "url"},
+            "website":                      {
+                "group": "",
+                "type": const.__SPL_T_URL__,
+                "itemtype": "http://schema.org/Organization", "itemprop": "url",
+                "options": {
+                    "Url_Prefix": "http://",
+                }
+            },
             "activity_summary":             {"write": False},
             "use_partner_credit_limit":     {"write": False},
             "is_coa_installed":             {"write": False},
