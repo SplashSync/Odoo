ba57639ca714139fed4cbbe99fc5fcd5a13bc84b
BadPixxel <eshop.bpaquier@gmail.com>
2025-10-10 19:12:09 +0200

    Remove unused imports in InvoiceStatusHelper class

--- splashsync/helpers/objects/invoices/status.py
+++ splashsync/helpers/objects/invoices/status.py
@@ -11,10 +11,6 @@
 #  For the full copyright and license information, please view the LICENSE
 #  file that was distributed with this source code.
 
-from splashpy import const, Framework
-from splashpy.componants import FieldFactory
-
-
 class InvoiceStatusHelper:
     """
     Odoo Invoice Status Helper
